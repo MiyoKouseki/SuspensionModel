182d781b7fce02c4411412cfed3e8edc252ca6cc
AyakaAShoda <ayaka.shoda0514@gmail.com>
2020-06-29 11:19:48 +0900

    models with the actuation efficiency of JGW-T2011661

--- simulation/O3etmx/SimulateFBsignals.py
+++ simulation/O3etmx/SimulateFBsignals.py
@@ -19,7 +19,7 @@ sys.path.append(noisedir)
 import noise
 
 # %% initial parameters
-EX = sysc0('dampedETMX')
+EX = sysc0('dampedETMX2')
 freq = np.linspace(0.01,10,num=1000)
 
 #%% Load noise
@@ -90,34 +90,64 @@ plt.legend()
 plt.xlabel('Frequency [Hz]')
 plt.ylabel('Displacement [m/rtHz]')
 plt.ylim([1.e-22,1.e-4])
-plt.savefig('./figs/dampedTM_disp.png')
+plt.savefig('./figs/dampedTM_disp2.png',bbox_inches="tight")
 
 
 # %%
 
-freeEX = sysc0('freeETMX')
+freeEX = sysc0('freeETMX2')
 TFTML = freeEX.TF('injTML','dispTML',freq)
 TFMNL = freeEX.TF('injMNL','dispTML',freq)
 Ftm = EX.TF('ISC_TML','fbTMLOCKLmon',freq)
 Fmn = EX.TF('ISC_TML','fbMNLOCKLmon',freq)
 
+#%% check TM, MN TF
+plt.loglog(freq,abs(TFTML)*1.e-6,label='TM')
+plt.loglog(freq,abs(TFMNL)*1.e-6,label='MN')
+plt.legend()
+plt.xlabel('Frequency [Hz]')
+plt.ylabel('TF [m/cnt]')
+plt.savefig('./figs/TFs_TM_MN_mpcnt.png',bbox_inches="tight")
 #%% check cross-over
-plt.loglog(freq,abs(Ftm*TFTML))
-plt.loglog(freq,abs(Fmn*TFMNL))
-plt.loglog(freq,abs(Ftm*TFTML+Fmn*TFMNL))
+plt.loglog(freq,abs(Ftm*TFTML),label='TM loop')
+plt.loglog(freq,abs(Fmn*TFMNL),label='MN loop')
+plt.loglog(freq,abs(Ftm*TFTML+Fmn*TFMNL), label='SUM')
+plt.legend()
+plt.xlabel('Frequency [Hz]')
+plt.ylabel('TF [m/cnt]')
+plt.savefig('./figs/ServoTFs_TM_MN.png',bbox_inches="tight")
 
-# %%
+# %% estimated Feedback signals
 
 FB_TM = total*abs(Ftm/(Ftm*TFTML+Fmn*TFMNL))
 FB_MN = total*abs(Fmn/(Ftm*TFTML+Fmn*TFMNL))
 
-plt.loglog(freq, FB_TM*1.e+6, label='Xdisp/TF_TML')
-plt.loglog(freq, FB_MN*1.e+6, label='Xdisp/TF_MNL')
+plt.loglog(freq, FB_TM*1.e+6, label='TM FBsig')
+plt.loglog(freq, FB_MN*1.e+6, label='MN FBsig')
+plt.legend()
+plt.xlabel('Frequency [Hz]')
+plt.ylabel('FBsig [cnt]')
+plt.ylim([0.1,1.e+5])
+plt.savefig('./figs/FBsignals2.png',bbox_inches="tight")
+
+#%% Naive test
+
+plt.loglog(freq, total/abs(TFTML)*1.e+6, label='Xdisp/TF_TML')
+plt.loglog(freq, total/abs(TFMNL)*1.e+6, label='Xdisp/TF_MNL')
 plt.legend()
 plt.ylim([0.1,1.e+5])
+plt.xlabel('Frequency [Hz]')
+plt.ylabel('FBsig [cnt]')
+plt.savefig('./figs/Disp_devided_by_TFs.png',bbox_inches="tight")
 
 # %%
 TFs = [TFgnd, TFgndHL, TFIMps, TFMNps, TFBFLVDT,TFIPLVDT,TFTML,TFMNL,Ftm,Fmn]
 import pickle
 with open('TFs.pkl', 'wb') as pklfile:
   pickle.dump(TFs , pklfile)
+#%%
+import pickle
+with open('TFs.pkl', 'rb') as pklfile:
+  [TFgnd, TFgndHL, TFIMps, TFMNps, TFBFLVDT,TFIPLVDT,TFTML,TFMNL,Ftm,Fmn] = pickle.load(pklfile)
+
+#%%
